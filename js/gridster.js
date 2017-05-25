c606998b255e59b62c747d47852c9f57cd006cc6
lyuzhicong <lyuzhicong@163.com>
2017-05-25 18:31:53 +0800

    基础文件

--- js/gridster.js
+++ js/gridster.js
@@ -1,22 +1,65 @@
 ;
-(function() {
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery', 'lodash'], factory);
+    } else {
+        factory(jQuery, _);
+    }
+})(function($, _) {
     class Gridster {
-        constructor(node) {
+        constructor(node, options) {
             this.node = node;
+            this.options = options;
+            init();
         }
 
-        static init(options) {
-            let node = this.node;
-            if (typeof node == 'object') {
-                this.node = node;
-            } else {
-                this.node = document.getElementsByClassName(node);
+        static init() {
+
+            var node = $(this.node);
+            node.children().attr("class", "grid");
+
+            /*let options = {
+                base_dimensions: [],
+                autogrow_cols: false,
+                max_cols: 3,
+                min_cols: 1,
+                avoid_overlapped_widgets: true,
+                draggable: {
+                    start: {},
+                    drag: {},
+                    stop: {}
+                },
+                resize: {
+                    enabled: false,
+                    max_size: [],
+                    resize: {},
+                    stop: {}
+                }
+            };*/
+
+
+
+
+
+        }
+
+        _prepare_node = (node, resize) => {
+
+        }
+
+
+
+    }
+
+    var drag = () => {
+
+    }
+
+    $.fn.gridster = opts => {
+        return this.each(function() {
+            if (!$(this).data('gridster')) {
+                $(this).data('gridster', new Gridster(this, opts));
             }
-
-
-
-
-        }
-
+        });
     }
 })
